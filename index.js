98f54059640b30dc640ddc78ad292bfceb755155
Martins100-Tmd <olaitanfabayo@gmail.com>
2024-11-01 17:34:48 +0100

    Improvised

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ var typed = new Typed('#job', {
 const swiper = new Swiper('.swiper', {
    direction: 'horizontal',
    loop: true,
-   slidesPerView: 'auto', // Ensures each image takes a slide space
+   slidesPerView: 'auto', // Ensures each image takes
 });
