9da2a9ce24dca2ba8fa3c9ccae786dfb6a1bb91d
Martins100-Tmd <olaitanfabayo@gmail.com>
2024-11-01 17:34:32 +0100

    Improvised

--- index.js
+++ index.js
@@ -18,16 +18,4 @@ const swiper = new Swiper('.swiper', {
    direction: 'horizontal',
    loop: true,
    slidesPerView: 'auto', // Ensures each image takes a slide space
-   spaceBetween: 10, // Adds some space between slides
-   pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-   },
-   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-   },
-   scrollbar: {
-      el: '.swiper-scrollbar',
-   },
 });
