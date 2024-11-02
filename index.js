9b48149fd2635245f92b64270bab120193ba8bc7
Martins100-Tmd <olaitanfabayo@gmail.com>
2024-11-02 12:08:56 +0100

    Improvised

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ function updateListCursor(e) {
    element.classList.add('tag');
 }
 
+onload = () => {
+   let path = location.pathname.split('/')[1].split('.')[0];
+   let element = document.getElementById(path);
+   console.log(path, element);
+   element.classList.add('tag');
+};
+
+function reLocate(path) {
+   window.location.pathname = path;
+}
+
 var typed = new Typed('#job', {
    strings: ["I'm a Frontend Developer", "I'm a Backend developer", "I'm a fullstack Developer"],
    typeSpeed: 100,
